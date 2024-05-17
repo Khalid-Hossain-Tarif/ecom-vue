1dec53d399fb0bc7be117bc5e210381efb44aeec
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2024-05-17 16:26:08 +0600

    auth file updated

--- src/store/auth/store.js
+++ src/store/auth/store.js
@@ -4,58 +4,60 @@ import { apiBaseUrl } from "@/composables/baseApiUrl.js";
 import router from "../../router/index.js";
 
 const authStore = reactive({
-  isAuthenticated: localStorage.getItem('auth') == 1,
-  user: JSON.parse(localStorage.getItem('user')),
-
+  isAuthenticated: localStorage.getItem("auth") == 1,
+  user: JSON.parse(localStorage.getItem("user")),
 
   registration(email, username, password) {
-    axios.post(apiBaseUrl + "/users", {
-      email: email,
-      name: username,
-      password: password,
-      // confirmPassword: confirmPassword
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => {
-      console.log('clicked1')
-      console.log(res)
-      console.log('clicked2')
-    })
-    .catch(err => {
-      console.error('Error:', err);
-    });
+    axios
+      .post(apiBaseUrl + "/users", {
+          email: email,
+          name: username,
+          password: password,
+          // confirmPassword: confirmPassword
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        router.push("/login");
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      })
   },
 
   authenticate(email, password) {
-    axios.post(apiBaseUrl + "/login", {
-      email: email,
-      password: password
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => {
-      if(res?.data?.success == 0) {
-        authStore.isAuthenticated = true;
-        authStore.user = res?.data;
-        localStorage.setItem('auth', 1);
-        localStorage.setItem('user', JSON.stringify(authStore.user));
-        router.push("/");
-      }
-    })
-    .catch(err => {
-      console.error('Error:', err);
-    });
+    axios
+      .post(apiBaseUrl + "/login", {
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        if (res?.data?.success == 0) {
+          authStore.isAuthenticated = true;
+          authStore.user = res?.data;
+          localStorage.setItem("auth", 1);
+          localStorage.setItem("user", JSON.stringify(authStore.user));
+          router.push("/");
+        }
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      })
   },
   logout() {
     authStore.isAuthenticated = false;
-    authStore.user = '',
-    localStorage.setItem('auth', 0);
-    localStorage.setItem('user', '{}');
+    (authStore.user = ""), localStorage.setItem("auth", 0);
+    localStorage.setItem("user", "{}");
     router.push("/login");
   },
 });
